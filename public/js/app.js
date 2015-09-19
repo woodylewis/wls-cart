f8895be3491ef3aadda969644665a6c7f880a7c5
woodylewis <woody@woodylewis.com>
2015-09-19 13:51:37 -0400

    First display

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,4 @@
 angular.module('stride-test', [
 	'ui.router',
 	'stride-test.mainCtrl'
-])
-.config(['$stateProvider', '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider ) {
-
-	$urlRouterProvider
-	.otherwise('/index');
-	
-	$stateProvider
-    .state('main', {
-      url: "/main",
-      views: {
-        "state" : { templateUrl: "partials/main.html" }
-      }
-    });
-}]);
+]);
--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -3,10 +3,26 @@
 angular.module('stride-test.mainCtrl', [
 	'stride-test.inventoryService'
 ])
-.controller('mainCtrl', ['$scope', 'inventoryService', function($scope, inventoryService) {
+.config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider ) {
+
+	$urlRouterProvider
+	.otherwise('/index');
+	
+	$stateProvider
+    .state('main', {
+      url: "/main",
+      views: {
+        "state" : { templateUrl: "partials/main.html" }
+      }
+    });
+}])
+.controller('mainCtrl', ['$scope', '$state', 'inventoryService', function($scope, $state, inventoryService) {
 	inventoryService.fetchInventory()
-	.then(function (inventory) {
-		console.log(inventory);
+	.then(function (data) {
+		$scope.chocolates = data;
+		console.log($scope.chocolates);
+    	$state.go('main');
 	}),
 	function (error) {
 		console.log('error on fetchInventory - ', error);

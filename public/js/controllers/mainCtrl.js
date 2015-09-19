4fdf8e6dda27103ccdb404786a5055c73195c0fa
woodylewis <woody@woodylewis.com>
2015-09-19 13:06:43 -0400

    Add service

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('stride-test.mainCtrl', [
+	'stride-test.inventoryService'
 ])
 .controller('mainCtrl', ['$scope', function($scope) {
 	console.log('mainCtrl ');
---
+++ public/js/services/inventoryService.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('stride-test.inventoryService', [])
+/* SERVICE TO WRAP HTTP REQUESTS */
+.factory('inventoryService' , ['$q', '$http', 
+	function($q, $http) {
+		var invetoryUrl = 'http://localhost:3000/data/inventory.json';
+
+		var fetchInventory = function() {
+			var deferred = $q.defer();
+
+			$http.get(invetoryUrl)
+			.success( function(data) {
+				deferred.resolve(data);
+			})
+			.error(function(reason) {
+				deferred.reject(reason);
+			});
+			return deferred.promise;
+		};
+
+	return {
+		fetchInventory: function() {
+			return fetchInventory();
+		}
+	};
+}]);

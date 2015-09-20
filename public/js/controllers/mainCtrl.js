39c083d22822ce1ede21ad1b4d30c6471778474d
woodylewis <woody@woodylewis.com>
2015-09-20 14:18:05 -0400

    View Cart and Add to Cart buttons, styling

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -6,11 +6,14 @@ angular.module('stride-test.mainCtrl', [
 ])
 .controller('mainCtrl', ['$scope', 'inventoryService', function($scope, inventoryService) {
 	$scope.showCart = false;
+	$scope.numItems = 0;
 	$scope.milk=0;
 	$scope.dark=0;
 	$scope.white=0;
 	$scope.sugarfree=0;
 	$scope.total = 1000;
+	
+	//---Read data from the json file
 	inventoryService.fetchInventory()
 	.then(function (data) {
 		$scope.chocolates = data;
